8bb0b102f78a775d07fd9b5d390199a527a23d4d
NguyenDoanKhoaVan_21211TT3156.github.io <kenvan1806@gmail.com>
2024-10-17 08:31:14 +0700

    lab4

---
+++ front/3055/mau-content.php
@@ -0,0 +1,57 @@
+<div class="header">
+        <div class="icon-text-right">
+            <div>
+                <i class="fas fa-tools"></i>
+                <span>WE REPAIR ALL TYPES OF ELECTRONICS</span>
+            </div>
+            <div>
+                <i class="fas fa-map-marker-alt"></i>
+                <span>ENTER THE DELIVERY ADDRESS</span>
+            </div>
+            <div>
+                <i class="fas fa-stethoscope"></i>
+                <span>FREE DIAGNOSTICS AND COST EVALUATION</span>
+            </div>
+            <div>
+                <i class="fas fa-cogs"></i>
+                <span>HIGH QUALITY SPARE PARTS ONLY</span>
+            </div>
+        </div>
+    </div>
+    <div class="services">
+        <div class="services-header">
+            <h2>OUR SERVICES</h2>
+            <div class="navigation">
+                <i class="fas fa-chevron-left"></i>
+                <i class="fas fa-chevron-right"></i>
+            </div>
+        </div>
+        <table>
+            <tr>
+                <td>
+                    <img src="https://placehold.co/300x200" alt="Game console on a wooden table"/>
+                    <h3>GAME CONSOLES REPAIR</h3>
+                    <p>
+                        Nullam non dignissim est, interdum ornare arcu. Morbi dignissim a ipsum in dignissim. Cras semper magna nec suscipit fermentum. Nullai iaculis dolor eu finibus imperdiet. Nullam non dignissim est, interdum ornare arcu.
+                    </p>
+                    <a href="#">READ MORE</a>
+                </td>
+                <td>
+                    <img src="https://placehold.co/300x200" alt="Hand holding a phone with a broken screen"/>
+                    <h3>WIFI PROBLEMS</h3>
+                    <p>
+                        Phasellus dui ligula, bibendum faucibus commodo at, porta non nunc. Sed turpis tortor, lobortis at tristique sed, interdum nec neque.
+                    </p>
+                    <a href="#">READ MORE</a>
+                </td>
+                <td>
+                    <img src="https://placehold.co/300x200" alt="Water damaged phone"/>
+                    <h3>WATER DAMAGE</h3>
+                    <p>
+                        Fusce mollis turpis quis est sollicitudin bibendum. Donec laoreet leo dui, laoreet cursus metus pharetra ac. Proin condimentum dui at ipsum tempus volutpat eget eget lorem. Fusce mollis turpis quis est sollicitudin bibendum.
+                    </p>
+                    <a href="#">READ MORE</a>
+                </td>
+            </tr>
+        </table>
+    </div>
